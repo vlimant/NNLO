446fa57fc8457a655fecb7166a6eb9befdf0a0c9
Thong <thongnguyen1292@gmail.com>
2018-01-22 11:54:35 -0800

    Add condition for git clone

--- setup.sh
+++ setup.sh
@@ -1,2 +1,7 @@
-git clone git@github.com:duanders/mpi_learn.git
+if [ -d "mpi_learn" ]; then
+    git clone git@github.com:duanders/mpi_learn.git
+fi
+cd mpi_learn
+git pull
+cd ..
 touch mpi_learn/__init__.py
