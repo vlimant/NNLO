cf8d4378c88ad62c12c5cc525e85fee7b443ee30
Thong <thongnguyen1292@gmail.com>
2018-01-23 06:09:50 -0800

    Rename to option 0

--- hyperparameter_search_option0.py
+++ hyperparameter_search_option0.py
@@ -4,7 +4,7 @@ import time
 import mpiLAPI
 import numpy as np
 import json
-
+import os
 
 def dummy_model(params):
     import random
@@ -48,8 +48,9 @@ def train_model_dummy(params):
         print("CalledProcessError: {}".format(err))
 
 
-def train_model(params, n_nodes=3, n_epochs=3):
-    model = mpiLAPI.get_model(depth=3*params[0]+4, growth_rate=params[1], dropout_rate=params[2], nb_filter=params[3], lr=10.0**np.int32(params[4]))
+def train_model(params, n_nodes=1, n_epochs=3):
+    #model = mpiLAPI.test_densenet(depth=3*params[0]+4, growth_rate=params[1], dropout_rate=params[2], nb_filter=params[3], lr=10.0**np.int32(params[4]))
+    model = mpiLAPI.test_cnn(dropout=params[0], kernel_size=params[1], lr=10.**np.int32(params[2]))   
     mlapi = mpiLAPI.mpi_learn_api( model = model,
                            train_pattern = '/bigdata/shared/LCDJets_Remake/train/04*.h5',
                            val_pattern = '/bigdata/shared/LCDJets_Remake/val/020*.h5',
@@ -60,7 +61,7 @@ def train_model(params, n_nodes=3, n_epochs=3):
                     trial_name = 'test',
                     features_name = 'Images',
                     labels_name = 'Labels',
-                    batch = 4,
+                    batch = 2,
                     epoch = n_epochs,
                     verbose = True,
                     loss = 'categorical_crossentropy',
@@ -85,18 +86,25 @@ def train_model(params, n_nodes=3, n_epochs=3):
 
 
 if __name__ == '__main__':
-    n_trials = 20
+    n_trials = 4
+    os.environ["CUDA_VISIBLE_DEVICES"]="4,5"
+
     concurrent_train = 5
-    param_grid = [
+    param_grid_cnn = [
+        (.0, .85),  # dropout_rate
+        (3,10),  # kernel_size
+        (-5, 1),  # lr
+    ]
+    param_grid_densenet = [
         (3, 8),  # depth
         (1, 5),  # growth_rate
-        (.0, .5),  # dropout_rate
+        (.0, .85),  # dropout_rate
         (32, 256),  # nb_filters
         (-5, 1),  # lr
     ]
 
     # https://scikit-optimize.github.io/#skopt.Optimizer
-    bayesian_opt = Optimizer(param_grid)
+    bayesian_opt = Optimizer(param_grid_cnn)
 
     # TODO: how many do you want to spawn at the start?
     for i in range(n_trials):
