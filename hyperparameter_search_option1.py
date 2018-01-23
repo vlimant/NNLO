656fe3c873ac9a58628adb612f2f4defb8b345e2
Thong <thongnguyen1292@gmail.com>
2018-01-23 10:29:54 -0800

    Option 1 async now call the same mpilapi

--- hyperparameter_search_option1.py
+++ hyperparameter_search_option1.py
@@ -56,38 +56,6 @@ def get_fom_from_json_file(fname):
         return float(fom)
 
 
-def train_model(params, n_nodes=3, n_epochs=3):
-    model = mpiLAPI.get_model(depth=3*params[0]+4, growth_rate=params[1], dropout_rate=params[2], nb_filter=params[3], lr=10.0**np.int32(params[4]))
-    mlapi = mpiLAPI.mpi_learn_api( model = model,
-                           train_pattern = '/bigdata/shared/LCDJets_Remake/train/04*.h5',
-                           val_pattern = '/bigdata/shared/LCDJets_Remake/val/020*.h5',
-                           check_file = True
-                           )
-    try:
-        output = mlapi.train(N=n_nodes,
-                    trial_name = 'test',
-                    features_name = 'Images',
-                    labels_name = 'Labels',
-                    batch = 4,
-                    epoch = n_epochs,
-                    verbose = True,
-                    loss = 'categorical_crossentropy',
-                    easgd = False,
-                    early_stopping = 5
-                    )
-        search_str = "Wrote trial information"
-        for line in output.split('\n'):
-            print("search output: " + line)
-
-            if line.startswith(search_str):
-                print("found line: " + line)
-                fname = line.split(' ')[-1]
-                return get_fom_from_json_file(fname)
-    except subprocess.CalledProcessError as err:
-        print("CalledProcessError: {}".format(err))
-        return np.inf
-
-
 def get_train_cmd(self, **args):
     com = 'mpirun -n %d mpi_learn/MPIDriver.py %s %s %s'%(
         args.get('N', 2),
@@ -118,31 +86,41 @@ def get_train_cmd(self, **args):
     print(com)
     return com
 
+def train_model_async(params, n_nodes=3, n_epochs=3, trial_id=0, model_name='cnn_arch'):
+    model = mpiLAPI.test_cnn(dropout=params[0], kernel_size=params[1], lr=10.**np.int32(params[2]))   
 
-def train_model_async(params, n_nodes=3, n_epochs=3, trial_id=0):
-    n_nodes = "1"
-    model_arch_files = "mnist_arch.json"
-    train_files = "train_mnist.list"
-    test_files = "test_mnist.list"
-    n_epochs = "1"
-
-    # model = build_model(params)
-    # json_model = model.to_json()
-    json_model = "{}"
-
-    mpi_cmd = "mpirun"  # "mpirun"
-    train_cmd = mpi_cmd + " -np " + n_nodes + " python ./mpi_learn/MPIDriver.py " + model_arch_files + " " + train_files + " " + \
-        test_files + " --loss categorical_crossentropy --tf --epochs " + n_epochs + " --trial-name " + str(trial_id)
-
-    print("starting MPI process asynchronously.")
-    print(train_cmd)
+    print("Starting MPI process asynchronously.")
+    mlapi = mpiLAPI.mpi_learn_api( model = model,
+                           train_pattern = '/bigdata/shared/LCDJets_Remake/train/04*.h5',
+                           val_pattern = '/bigdata/shared/LCDJets_Remake/val/020*.h5',
+                           check_file = True,
+                           nohash = True,
+                           model_name = model_name
+                           )
+    
     time.sleep(1)  # don't overload the cluster with mpi_run calls
+    
+    output = mlapi.train(N=n_nodes,
+                trial_name = str(trial_id),
+                features_name = 'Images',
+                labels_name = 'Labels',
+                batch = 4,
+                epoch = n_epochs,
+                verbose = True,
+                loss = 'categorical_crossentropy',
+                easgd = False,
+                early_stopping = 5,
+                get_output = False,
+                )
 
-    import tempfile
-    tfil = tempfile.TemporaryFile()
+#    json_model = "{}"
+#
+#    mpi_cmd = "mpirun"  # "mpirun"
+#    train_cmd = mpi_cmd + " -np " + n_nodes + " python3 ./mpi_learn/MPIDriver.py " + model_arch_files + " " + train_files + " " + \
+#        test_files + " --loss categorical_crossentropy --tf --epochs " + n_epochs + " --trial-name " + str(trial_id)
 
-    p = subprocess.Popen(train_cmd, shell=True, stdout=tfil, stderr=tfil)
-    return p
+    #p = subprocess.Popen(train_cmd, shell=True, stdout=tfil, stderr=tfil)
+    return output
 
 
 def get_finished_process(proc_list):
@@ -158,17 +136,22 @@ def get_finished_process(proc_list):
 if __name__ == '__main__':
     n_trials = 10
     concurrent_train = 5
-    model_name = "mnist_arch"
-    param_grid = [
+    model_name = "cnn_arch"
+    param_grid_cnn = [
+        (.0, .85),  # dropout_rate
+        (3,10),  # kernel_size
+        (-5, 1),  # lr
+    ]
+    param_grid_densenet = [
         (3, 8),  # depth
         (1, 5),  # growth_rate
-        (.0, .5),  # dropout_rate
+        (.0, .85),  # dropout_rate
         (32, 256),  # nb_filters
         (-5, 1),  # lr
     ]
 
     # https://scikit-optimize.github.io/#skopt.Optimizer
-    bayesian_opt = Optimizer(param_grid)
+    bayesian_opt = Optimizer(param_grid_cnn)
 
     # TODO: how many do you want to spawn at the start?
     running_proc = []
@@ -179,7 +162,7 @@ if __name__ == '__main__':
             print("new run: " + str(run_id))
             suggested = bayesian_opt.ask()
             print(suggested)
-            p_i = train_model_async(suggested, n_epochs=1, n_nodes=1, trial_id=run_id)
+            p_i = train_model_async(suggested, n_epochs=1, n_nodes=1, trial_id=run_id, model_name = model_name)
             print((run_id, suggested, p_i))
             running_proc.append((run_id, suggested, p_i))
             run_id += 1
