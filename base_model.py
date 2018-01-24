2524e5f544967fdafb44c8a88f529ad273ab6d3f
Thong <thongnguyen1292@gmail.com>
2018-01-24 06:49:57 -0800

    Replace Sequential with Model and remove FunctionalModel func

--- base_model.py
+++ base_model.py
@@ -28,70 +28,6 @@ class CNNModel(BaseModel):
         dropout = params[1]
         lr = 10.0 ** params[2]
 
-        model = Sequential()
-        model.add(Conv2D(32, kernel_size=(kernel_size, kernel_size), strides=3, activation='relu', input_shape=self.input_shape))
-        model.add(Conv2D(32, kernel_size=(kernel_size, kernel_size), strides=3, activation='relu'))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
-        model.add(Dropout(dropout / 2))
-
-        model.add(Flatten())
-        model.add(Dense(128, activation='relu'))
-        model.add(Dropout(dropout))
-        model.add(Dense(3, activation='softmax'))
-        model.compile(loss='categorical_crossentropy', optimizer=Adam(lr))
-        return model.to_json()
-
-    def get_parameter_grid(self):
-        return [
-            (3, 9),   # kernel_size
-            (.0, .5), # dropout
-            (-5, 1),  # lr
-        ]
-    
-    def get_name(self):
-        return "CNN_model"
-
-class DenseNetModel(BaseModel):
-    def __init__(self, input_shape=(150,94,5)):
-        self.input_shape = input_shape
-        return
-    
-    def build(self, params):
-        depth = params[0]
-        nb_dense_block = params[1]
-        growth_rate = params[2]
-        dropout_rate = params[3]
-        nb_filter = params[4]
-        lr = 10. ** params[5]
-        densenet = DenseNet(nb_classes=3, img_dim=self.input_shape, depth=depth, nb_dense_block=nb_dense_block, 
-                        growth_rate = growth_rate, dropout_rate = dropout_rate, nb_filter = nb_filter)
-        optimizer = Adam(lr = lr)
-        densenet.compile(loss='categorical_crossentropy', optimizer = optimizer)
-        return densenet.to_json()
-
-    def get_parameter_grid(self):
-        return [
-            (10, 10),  # depth
-            (3, 3),    # nb_dense_block
-            (12, 12),  # growth_rate
-            (.0, .0),  # dropout 
-            (16, 16),  # nb_filter
-            (-5, 1),   # lr
-        ]
-        
-    def get_name(self):
-        return "DenseNet"
-
-class FunctionalModel(BaseModel):
-    def __init__(self, input_shape=(150,94,5)):
-        self.input_shape = input_shape
-        return
-
-    def build(self, params):
-        kernel_size = params[0]
-        dropout = params[1]
-        lr = 10.0 ** params[2]
-
         inputs = Input(shape=(150,94,5))
         x = Dense(64, activation='relu')(inputs)
         x = Dense(64, activation='relu')(x)
@@ -125,9 +61,41 @@ class FunctionalModel(BaseModel):
     
     def get_name(self):
         return "CNN_model"
+       return "CNN_model"
+
+class DenseNetModel(BaseModel):
+    def __init__(self, input_shape=(150,94,5)):
+        self.input_shape = input_shape
+        return
+    
+    def build(self, params):
+        depth = params[0]
+        nb_dense_block = params[1]
+        growth_rate = params[2]
+        dropout_rate = params[3]
+        nb_filter = params[4]
+        lr = 10. ** params[5]
+        densenet = DenseNet(nb_classes=3, img_dim=self.input_shape, depth=depth, nb_dense_block=nb_dense_block, 
+                        growth_rate = growth_rate, dropout_rate = dropout_rate, nb_filter = nb_filter)
+        optimizer = Adam(lr = lr)
+        densenet.compile(loss='categorical_crossentropy', optimizer = optimizer)
+        return densenet.to_json()
+
+    def get_parameter_grid(self):
+        return [
+            (10, 10),  # depth
+            (3, 3),    # nb_dense_block
+            (12, 12),  # growth_rate
+            (.0, .0),  # dropout 
+            (16, 16),  # nb_filter
+            (-5, 1),   # lr
+        ]
+        
+    def get_name(self):
+        return "DenseNet"
 
 
 if __name__ == '__main__':
-    arch = FunctionalModel()
+    arch = CNNModel()
     m = arch.build([0, 0, 0])
     print(m)
