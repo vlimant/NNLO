4ef2fcaff5f5ff609b235db4370cd5ad9928abe9
Thong <thongnguyen1292@gmail.com>
2018-06-19 08:41:00 -0700

    Protect from division by zero

--- PytorchCNN.py
+++ PytorchCNN.py
@@ -16,9 +16,13 @@ class _ConvBlock(nn.Sequential):
     def __init__(self, conv_layers, dropout, in_ch=5):
         super().__init__()
         for i in range(conv_layers):
-            self.add_module('convlayer%d'%(i), nn.Conv2d(in_ch*((i+1)%5), out_channels=in_ch*((i+2)%5),kernel_size=(3,3),stride=1, padding=1))
+            channel_in = in_ch*((i+1)%5)
+            channel_out = in_ch*((i+2)%5)
+            if channel_in == 0: channel_in += 1
+            if channel_out == 0: channel_out += 1
+            self.add_module('convlayer%d'%(i), nn.Conv2d(channel_in, out_channels=channel_out,kernel_size=(3,3),stride=1, padding=1))
             self.add_module('relu%d'%(i), nn.ReLU(inplace=True))
-        self.add_module('convlayer%d'%(conv_layers), nn.Conv2d(in_ch*((conv_layers+1)%5), out_channels=1, kernel_size=(3,3), stride=2, padding=1))
+        self.add_module('convlayer%d'%(conv_layers), nn.Conv2d(channel_out, out_channels=1, kernel_size=(3,3), stride=2, padding=1))
         self.dropout = dropout
 
     def forward(self, x):
@@ -31,7 +35,7 @@ class _DenseBlock(nn.Sequential):
     def __init__(self, dense_layers, dropout):
         super().__init__()
         for i in range(dense_layers):
-            self.add_module('denselayer%d'%(i), nn.Linear(10000//(2**i), 10000//(2**(i+1))))
+            self.add_module('denselayer%d'%(i), nn.Linear(int(10000//(2**i)), int(10000//(2**(i+1)))))
             self.add_module('relu%d'%(i), nn.ReLU(inplace=True))
         self.dropout = dropout
 
@@ -50,7 +54,7 @@ class CNN(nn.Module):
         self.conv_layers = _ConvBlock(args[0], args[2], args[4])
         self.dense_layers = _DenseBlock(args[1], args[2])
         self.adapt_pool = nn.AdaptiveMaxPool2d((100,100))
-        self.output = nn.Linear(10000//(2**args[1]), args[3])
+        self.output = nn.Linear(int(10000//(2**args[1])), int(args[3]))
 
     def forward(self, x):
         x = self.conv_layers(x)
