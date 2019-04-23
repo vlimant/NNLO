56db0ceae81aef384d9803b1a78a49289913d399
Jean-Roch Vlimant <vlimant@cern.ch>
2019-04-23 05:43:15 -0700

    cleanup

--- my_model.py
+++
@@ -1,9 +0,0 @@
-# Define your model here
-
-def get_model():
-    """Should return the model object (keras.models.Model or torch.nn.Module)."""
-    raise NotImplementedError
-
-def get_name():
-    """String containing model name (optional)."""
-    raise NotImplementedError
