b75261daf75199e644b3b7f25f874a7fd2e89c26
Jean-Roch Vlimant <vlimant@cern.ch>
2019-10-17 07:22:58 -0700

    popyey path

--- examples/example_jedi_torch.py
+++ examples/example_jedi_torch.py
@@ -171,6 +171,8 @@ def get_all():
         all_list = glob.glob('/ccs/proj/csc291/DATA/hls-fml/NEWDATA/jetImage*_150p_*.h5')
     elif 'workergpu' in host or 'rusty' in host:
         all_list = glob.glob('/mnt/ceph/users/vlimant/hls-fml/jetImage*_150p_*.h5')
+    elif 'popeye' in host or 'pcn' in host:
+        all_list = glob.glob('/simons/scratch/vlimant/JEDI/jetImage*_150p_*.h5')
     else:
         all_list = glob.glob('/storage/group/gpu/bigdata/hls-fml/NEWDATA/jetImage*_150p_*.h5')
     #self.X =  np.array(self.f.get('jetConstituentList'))
